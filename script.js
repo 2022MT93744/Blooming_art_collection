0a848fb736a78e602c15d728f2438f391533b504
2022MT93744 <127505741+2022MT93744@users.noreply.github.com>
2024-04-09 00:33:55 +0530

    added all the updated 09042024

--- script.js
+++ script.js
@@ -1,22 +1,81 @@
 const snakep = 'The snake plant, also known as Sansevieria, is a popular indoor plant characterized by its long, upright leaves that resemble snakes. It is highly adaptable to various light conditions and requires minimal maintenance, making it ideal for beginners. Snake plants purify indoor air by removing toxins and can thrive in low light environments.';
 const moneyp = 'The money plant, also known as Epipremnum aureum, is a popular indoor plant with heart-shaped leaves. It is believed to bring prosperity, luck, and wealth according to Feng Shui. Easy to care for, it thrives in indirect light and can purify indoor air, making it a favorite among plant enthusiasts.';
-const www = 'Money to spaces.';
-const aa = 'Money to spaces.';
-const ee = 'Money to spaces.';
-const wwww = 'Money to spaces.';
-const ss = 'Money to spaces.';
+const neonp = 'The Neon Money Plant, also known as Epipremnum aureum (Neon), is a vibrant and eye-catching houseplant known for its striking neon green leaves. It thrives in indirect light and requires minimal care, making it ideal for beginners. Its air-purifying qualities and unique coloration make it a popular choice for indoor spaces.';
+const spiderp = 'The Spider Plant, scientifically known as Chlorophytum comosum, is a popular indoor plant renowned for its air-purifying qualities and easy care. Characterized by long, arching leaves with white stripes, it thrives in bright, indirect light and well-draining soil. Spider plants are resilient, adaptable, and produce small white flowers and baby plantlets, making them a favorite choice for beginner gardeners.';
+const zzp = 'The ZZ plant, scientifically known as Zamioculcas zamiifolia, is a popular indoor plant valued for its low maintenance and air-purifying qualities. With glossy, dark green leaves and a compact growth habit, it thrives in low light conditions and requires infrequent watering, making it an ideal choice for busy plant lovers.';
+const reop = 'The Reo Plant, also known as Sansevieria Trifasciata or Snake Plant, is a popular indoor plant cherished for its air-purifying qualities and low maintenance. With its long, sword-shaped leaves and striking appearance, it adds a touch of greenery to any space while requiring minimal watering and care, making it ideal for busy plant enthusiasts.';
+const jewp = 'The Wandering Jew plant, scientifically known as Tradescantia zebrina, is a popular houseplant cherished for its vibrant purple and green foliage. It is easy to grow, requiring moderate light and regular watering. Its trailing vines make it ideal for hanging baskets, adding a touch of color and beauty to indoor spaces.';
+const brokheartp = 'The Philodendron Broken Heart plant, scientifically known as Philodendron hederaceum (Micans), is an exquisite foliage plant. Its heart-shaped leaves exhibit stunning shades of green, bronze, and copper, creating a captivating visual display. With proper care and indirect light, this plant thrives indoors, adding a touch of elegance and beauty to any space.';
+const mcrownthronp = 'The miniature crown of thorns plant, scientifically known as Euphorbia milii, is a charming succulent with tiny, thorny stems and clusters of colorful flowers. It thrives in bright sunlight and well-draining soil, making it a popular choice for indoor gardens. With proper care, this plant adds beauty and elegance to any space.';
+const hoyap = 'Hoya plants, also known as wax plants, are popular for their waxy leaves and unique star-shaped flowers. They are low-maintenance and thrive in indirect light, making them ideal for indoor spaces. With over 200 species, Hoya plants offer a variety of leaf shapes and colors, adding beauty and charm to any home.';
+const devilbbp = "Devil's Backbone (Pedilanthus tithymaloides) is a resilient and distinctive succulent plant with thick, zigzag stems that resemble a spine. Native to Madagascar, it thrives in bright, indirect light and well-draining soil. Its unique appearance, low maintenance requirements, and drought tolerance make it a popular choice for indoor and outdoor gardens.";
+const dwarfepalmp = "The Dwarf Areca Palm, scientifically known as Dypsis lutescens 'Compacta,' is a popular indoor plant prized for its elegant appearance and air-purifying qualities. It features graceful, arching fronds and a compact growth habit, making it ideal for smaller spaces. This palm thrives in bright, indirect light and requires regular watering to maintain its lush green foliage. With proper care, the Dwarf Areca Palm adds a tropical touch to any indoor environment while contributing to better air quality.";
+const portulecap= "Portulaca, also known as moss rose or purslane, is a low-growing succulent plant with colorful, cup-shaped flowers. It thrives in hot, dry conditions and is often used as a ground cover or in hanging baskets. Portulaca is easy to grow, drought-tolerant, and attracts pollinators like bees and butterflies to the garden.";
+const aglonimap = "Aglaonema, commonly known as Chinese Evergreen, is a popular indoor plant valued for its striking foliage and air-purifying qualities. With vibrant green leaves accented by silver, red, or pink patterns, it adds beauty to any space. Low maintenance and adaptable to various light conditions, Aglaonema is ideal for indoor environments.";
+const patharchattap = "Patharchatta, scientifically known as Bryophyllum pinnatum, is a succulent plant native to Madagascar but widely distributed in tropical regions. It's valued in traditional medicine for its anti-inflammatory, antimicrobial, and wound-healing properties. Patharchatta has thick, fleshy leaves that store water, making it drought-resistant. It's easy to grow, requiring minimal care, and is used for various ailments like respiratory issues, skin problems, and digestive disorders.";
+const ajwinp = "Ajwain leaves, known for their pungent aroma, are used both in cooking and medicine. In culinary applications, they enhance the flavor of dishes like curries, chutneys, and bread. Medicinally, they are valued for their digestive properties, aiding in relieving indigestion, flatulence, and stomach discomfort. Ajwain leaves are versatile and beneficial additions to various cuisines and health remedies.";
+const dfsnakep = "The dwarf snake plant, scientifically known as Sansevieria trifasciata 'Hahnii,' is a compact and low-maintenance indoor plant. It features short, broad leaves with dark green variegation, making it an attractive choice for small spaces. Its air-purifying qualities and ability to thrive in low light conditions make it ideal for indoor environments.";
+const cylsnakep = "The cylindrical snake plant, scientifically known as Sansevieria cylindrica, is a unique and striking succulent known for its cylindrical, spear-shaped leaves. It is a low-maintenance indoor plant that thrives in indirect sunlight and requires minimal watering. Its architectural form adds a modern and elegant touch to any indoor space.";
+const donkyp = "The Donkey Ear plant, also known as Kalanchoe gastonis-bonnieri, is a succulent characterized by its thick, paddle-shaped leaves resembling donkey ears. It is prized for its striking appearance and low maintenance requirements, making it a popular choice for indoor and outdoor gardens. Its vibrant green foliage adds a refreshing touch to any space, and it thrives in well-draining soil with ample sunlight."
+const aloep = "Aloe vera, a succulent plant known for its medicinal properties, has thick, fleshy leaves containing a gel-like substance used for skincare, healing burns, and soothing skin irritations. It thrives in arid climates, requires minimal water, and can be grown indoors or outdoors, making it a popular and versatile plant choice."
+const luckbbp = "Lucky Bamboo, a symbol of prosperity and good fortune, is an easy-to-care-for indoor plant known for its elegant stalks and lush green foliage. Believed to bring positive energy and luck, it thrives in low light conditions and is often used in Feng Shui to attract wealth and happiness."
+const planter = "These planters are crafted by hand, featuring unique designs painted onto recycled plastic bottles. They showcase a creative approach to sustainability by repurposing materials, adding artistic flair to eco-friendly practices."
+const topiarp = "Topiary plants are meticulously trimmed and shaped to create stunning living sculptures. Their artful forms, ranging from geometric shapes to intricate designs, adorn gardens and indoor spaces alike. Beyond their aesthetic appeal, these plants often symbolize creativity, patience, and the harmonious blend of nature and human craftsmanship."
+const sydersnakep = "The Spider Plant, known for its long, arching foliage and air-purifying qualities, is easy to care for and thrives in indirect sunlight. Snake plants, with their upright, sword-shaped leaves, are resilient and excellent for beginners, tolerating low light and irregular watering. Both are popular indoor plants for their aesthetics and low maintenance."
 
 const imageInfo = [
-    { name: 'p1.jpg', title: 'Terracotta Pot', price: '$15.99', status:'Available', details:snakep },
-    { name: 'p1.jpg', title: 'Ceramic Pot', price: '$20.49', status:'ordered', details:moneyp },
-    { name: 'p1.jpg', title: 'Decorative Pot', price: '$18.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Hanging Pot', price: '$25.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Plant Pot Set', price: '$35.99', status:'Not Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Vintage Pot', price: '$22.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Modern Pot', price: '$28.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Colorful Pot', price: '$19.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Clay Pot', price: '$17.99', status:'Available', details:moneyp },
-    { name: 'p1.jpg', title: 'Indoor Pot', price: '$24.99', status:'Available', details:moneyp }
+	  //topiary plants
+    { name: 'p02.jpeg', title: "Topiary of Money plant", price: '₹2999', status:'Available', details:topiarp },
+    { name: 'p08.jpeg', title: "Topiary of Money plant & Spider plant", price: '₹999', status:'Available', details:topiarp },
+    { name: 'p18.jpeg', title: 'Spider Plant & Snake plant', price: '₹1099', status:'Available', details:sydersnakep },
+
+    //Big plants
+    { name: 'p01.jpeg', title: 'ZZ plant', price: '₹499', status:'Available', details:zzp },
+    { name: 'p07.jpeg', title: 'Ajwain plant', price: '₹499', status:'Available', details:ajwinp },
+    { name: 'p10.jpeg', title: 'Snake plant', price: '₹299', status:'Available', details:snakep },
+    { name: 'p30.jpeg', title: 'Snake plant', price: '₹299', status:'Available', details:snakep },
+    { name: 'p33.jpeg', title: "Devil's Backbone", price: '₹299', status:'Available', details:devilbbp },
+
+    //Small plants
+    { name: 'p04.jpeg', title: 'Reo Plant', price: '₹99', status:'Available', details:reop },
+    { name: 'p05.jpeg', title: "Devil's Backbone", price: '₹249', status:'Available', details:devilbbp },
+    { name: 'p09.jpeg', title: 'Cylindrical snake plant', price: '₹399', status:'Available', details:cylsnakep },
+    { name: 'p13.jpeg', title: 'Spider Plant', price: '₹99', status:'Available', details:spiderp },
+    { name: 'p19.jpeg', title: 'Dwarf Areca Palm', price: '₹299', status:'Available', details:dwarfepalmp },
+    { name: 'p20.jpeg', title: 'Hoya plants', price: '₹199', status:'Available', details:hoyap },
+	  { name: 'p21.jpeg', title: 'Patharchatta', price: '₹99', status:'Available', details:patharchattap },
+    { name: 'p22.jpeg', title: 'Aloe vera plant', price: '₹99', status:'Available', details:aloep },
+    { name: 'p23.jpeg', title: "Devil's Backbone", price: '₹199', status:'Available', details:devilbbp },
+    { name: 'p24.jpeg', title: 'Broken Heart plant', price: '₹249', status:'Available', details:brokheartp },
+    { name: 'p25.jpeg', title: 'Neon Money Plant', price: '₹249', status:'Available', details:neonp },
+    { name: 'p26.jpeg', title: 'Money plant', price: '₹199', status:'Available', details:moneyp },
+    { name: 'p27.jpeg', title: 'Aglaonema', price: '₹399', status:'Available', details:aglonimap },
+    { name: 'p28.jpeg', title: 'Spider Plant', price: '₹99', status:'Available', details:spiderp },
+    { name: 'p29.jpeg', title: 'Snake plant', price: '₹199', status:'Available', details:snakep },
+	  { name: 'p31.jpeg', title: 'Lucky Bamboo', price: '₹199', status:'Available', details:luckbbp },
+    { name: 'p32.jpeg', title: 'Crown of thorns plant', price: '₹149', status:'Available', details:mcrownthronp },
+    { name: 'p34.jpeg', title: 'ZZ plant', price: '₹249', status:'Available', details:zzp },
+    { name: 'p36.jpeg', title: 'Wandering Jew', price: '₹99', status:'Available', details:jewp },
+
+    //hanging plants
+    { name: 'p35.jpeg', title: 'Portulaca', price: '₹69', status:'Available', details:portulecap },
+    { name: 'p11.jpeg', title: 'Money plant', price: '₹99', status:'Available', details:moneyp },
+    { name: 'p14.jpeg', title: 'Crown of thorns plant', price: '₹299', status:'Available', details:mcrownthronp },
+    { name: 'p15.jpeg', title: 'Donkey Ear plant', price: '₹99', status:'Available', details:donkyp },
+    { name: 'p16.jpeg', title: 'Spider Plant', price: '₹499', status:'Available', details:spiderp },
+    
+    //planters
+    { name: 'p06.jpeg', title: 'Dwarf snake plant', price: '₹499', status:'Available', details:dfsnakep },
+    { name: 'p17.jpeg', title: 'Dwarf snake plant', price: '₹199', status:'Available', details:dfsnakep },
+    { name: 'p12.jpeg', title: 'Wandering Jew', price: '₹99', status:'Available', details:jewp },
+    { name: 'pl01.jpeg', title: 'Hand painted planters', price: '₹49', status:'Available', details:planter },
+    { name: 'pl02.jpeg', title: 'Hand painted planters', price: '₹49', status:'Available', details:planter },
+    { name: 'pl03.jpeg', title: 'Hand painted planters', price: '₹99', status:'Available', details:planter },
+    { name: 'p03.jpeg', title: 'Spider Plant', price: '₹299', status:'Available', details:spiderp },
+    { name: 'pl05.jpeg', title: 'Hand painted planters', price: '₹199', status:'Available', details:planter },
+    { name: 'pl06.jpeg', title: 'Hand painted planters', price: '₹199', status:'Available', details:planter },
+    { name: 'pl07.jpeg', title: 'Hand painted planters', price: '₹199', status:'Available', details:planter },
+    { name: 'pl04.jpeg', title: 'Hand painted planters', price: '₹89', status:'Available', details:planter }
   ];
   
   const galleryContainer = document.getElementById('imageGallery');
@@ -50,7 +109,7 @@ const imageInfo = [
   
   
     const button = document.createElement('button');
-    button.textContent = 'Add to Cart';
+    button.textContent = 'Details';
     button.addEventListener('click', () => addToCart(info));
   
     card.appendChild(img);
